ad1166b825d4a9a1644cddee293d743298c86029
Mohamed Abdirahman <minaj7435@gmail.com>
2026-04-25 17:30:34 +0300

    fix step10 order

--- main.py
+++ main.py
@@ -29,7 +29,7 @@
 # 
 # Major Hint: Look for the shared columns across tables you need to 'join' together.
 
-# In[31]:
+# In[43]:
 
 
 # CodeGrade step0
@@ -51,7 +51,7 @@ pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 # 
 # The company would like to let Boston employees go remote but need to know more information about who is working in that office. Return the first and last names and the job titles for all employees in Boston.
 
-# In[32]:
+# In[44]:
 
 
 # CodeGrade step1
@@ -67,7 +67,7 @@ df_boston = pd.read_sql("""
 # 
 # Recent downsizing and employee attrition have caused some mixups in office tracking and the company is worried they are supporting a 'ghost' location. Are there any offices that have zero employees?
 
-# In[33]:
+# In[45]:
 
 
 # CodeGrade step2
@@ -85,7 +85,7 @@ df_zero_emp = pd.read_sql("""
 # 
 # As a part of this larger analysis project the HR department is taking the time to audit employee records to make sure nothing is out of place and have asked you to produce a report of all employees. Return the employees first name and last name along with the city and state of the office that they work out of (if they have one). Include all employees and order them by their first name, then their last name.
 
-# In[34]:
+# In[46]:
 
 
 # CodeGrade step3
@@ -102,7 +102,7 @@ df_employee = pd.read_sql("""
 # 
 # There are several approaches you could take here, including a left join and filtering on null values or using a subquery to filter out customers who do have orders. In total there are 24 customers who have not placed an order.
 
-# In[35]:
+# In[47]:
 
 
 # CodeGrade step4
@@ -123,7 +123,7 @@ df_contacts = pd.read_sql("""
 # 
 # Hint: A member of their team mentioned that they are not sure the 'amount' column is being stored as the right datatype so keep this in mind when sorting.
 
-# In[36]:
+# In[48]:
 
 
 # CodeGrade step5
@@ -141,7 +141,7 @@ df_payment = pd.read_sql("""
 # 
 # The sales rep team has noticed several key team members that stand out as having trustworthy business relations with their customers, reflected by high credit limits indicating more potential for orders. The team wants you to identify these 4 individuals. Return the employee number, first name, last name, and number of customers for employees whose customers have an average credit limit over 90k. Sort by number of customers from high to low.
 
-# In[37]:
+# In[49]:
 
 
 # CodeGrade step6
@@ -160,7 +160,7 @@ df_credit = pd.read_sql("""
 # 
 # The product team is looking to create new model kits and wants to know which current products are selling the most in order to get an idea of what is popular. Return the product name and count the number of orders for each product as a column named 'numorders'. Also return a new column, 'totalunits', that sums up the total quantity of product sold (use the quantityOrdered column). Sort the results by the totalunits column, highest to lowest, to showcase the top selling products.
 
-# In[38]:
+# In[50]:
 
 
 # CodeGrade step7
@@ -181,7 +181,7 @@ df_product_sold = pd.read_sql("""
 # 
 # Hint: You might need to join more than 2 tables. Use DISTINCT to return unique/different values.
 
-# In[39]:
+# In[51]:
 
 
 # CodeGrade step8
@@ -199,7 +199,7 @@ df_total_customers = pd.read_sql("""
 # 
 # The custom relations team is worried they are not staffing locations properly to account for customer volume. They want to know how many customers there are per office. Return the count as a column named 'n_customers'. Also return the office code and city.
 
-# In[40]:
+# In[52]:
 
 
 # CodeGrade step9
@@ -220,7 +220,7 @@ df_customers = pd.read_sql("""
 # 
 # Hint: Start with the subquery, find all the products that have been ordered by 19 or less customers, consider adapting one of your previous queries.
 
-# In[41]:
+# In[53]:
 
 
 # CodeGrade step10
@@ -238,13 +238,13 @@ df_under_20 = pd.read_sql("""
         GROUP BY od2.productCode
         HAVING COUNT(DISTINCT o2.customerNumber) < 20
     )
-    ORDER BY e.firstName
+    ORDER BY e.lastName
 """, conn)
 
 
 # ### Close the connection
 
-# In[42]:
+# In[54]:
 
 
 # Run this cell without changes
