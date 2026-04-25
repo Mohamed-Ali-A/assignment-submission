a9626481ef51c65c7719b705356c4643ac5e9ef5
Mohamed Abdirahman <minaj7435@gmail.com>
2026-04-25 17:19:27 +0300

    replace main.py with joins lab

--- main.py
+++ main.py
@@ -1,249 +1,252 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Introduction
+# ## Introduction
 # 
-# In this lab assessment you will explore writing more advanced SQL queries aimed at analyzing data on a more granular level. You will be working with 3 different databases throughout the assessment.
-# - planets.db: Contains data pertaining to planets in our solar system
-# - dogs.db: Contains data pertaining to famous fictional dog characters
-# - babe_ruth.db: Contains data pertaining to Babe Ruth's baseball career statistics
+# In this lab assessment, you'll practice your knowledge of JOIN statements and subqueries, using various types of joins and various methods for specifying the links between them. One of the main benefits of using a relational database is the table relations that define them which allow you to access and connect data together via shared columns. By writing more advanced SQL queries that utilize joins and subqueries you can provide a deeper and more granular level of analysis and data retrieval.
 # 
-# SQL (Structured Query Language) provides powerful tools for manipulating and analyzing data in relational databases. Four key operations for working with data are filtering, ordering, limiting, and grouping. These operations can be combined in a single query to perform complex data analysis and extraction tasks, allowing for powerful and flexible data manipulation.
+# This assessment will continue looking at the familiar Northwind database that contains customer relationship management (CRM) data as well as employee and product data. You will take a deeper dive into this database in order to accomplish more advanced SQL queries that require you to access data from multiple tables at once. 
+# 
+# Imagine that you are working in an analyst role for the sales rep team. They have collaborated with the customer relations and the product teams to take a comprehensive look at the employee to customer pipeline in an attempt to find areas of improvement and potential growth. You have been asked to provide some specific data and statistics regarding this project.
 
 # ## Learning Objectives
 # 
-# * Retrieve a subset of records from a table using a WHERE clause
-# * Filter results using conditional operators
-# * Apply an aggregate function to the result of a query
-# * Order the results of your queries by using ORDER BY (ASC & DESC)
-# * Limit the number of records returned by a query using LIMIT
-# * Use Group BY statements in SQL to apply aggregate functions
+# You will be able to:
+# 
+# * Write SQL queries that make use of various types of joins
+# * Choose and perform whichever type of join is best for retrieving desired data
+# * Write subqueries to decompose complex queries
 
-# ## Part I: Basic Filtering
+# ## Database
 # 
-# You will begin by looking at the planets data to perform some basic filtering queries.
+# The database will be the customer relationship management (CRM) database, which has the following ERD.
 # 
-# Table Name: planets
+# ![Database-Schema.png](ERD.png)
+# 
+# ### Connect to the database
+# 
+# In the cell below we have provided the code to import both pandas and sqlite3 as well as define and create the connection to the database you will use. Also displayed is the schema and table names from the database. Use this information in conjunction with the ERD image above to assist in creating your SQL Queries.
+# 
+# Major Hint: Look for the shared columns across tables you need to 'join' together.
 
 # In[1]:
 
 
 # CodeGrade step0
-
 # Run this cell without changes
 
-import pandas as pd
+# SQL Library and Pandas Library
 import sqlite3
+import pandas as pd
 
-# Create the connection
-# Note the connect is 'conn1' since there will be multiple .db used
-conn1 = sqlite3.connect('planets.db')
+# Connect to the database
+conn = sqlite3.connect('data.sqlite')
 
-# Select all
-pd.read_sql("""SELECT * FROM planets; """, conn1)
+pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
+
+# ## Part 1: Join and Filter
 
 # ### Step 1
-# Return all the columns for planets that have 0 moons.
+# 
+# The company would like to let Boston employees go remote but need to know more information about who is working in that office. Return the first and last names and the job titles for all employees in Boston.
+
+# In[2]:
+
+
+# CodeGrade step1
+df_boston = pd.read_sql("""
+    SELECT e.firstName, e.lastName, e.jobTitle
+    FROM employees e
+    JOIN offices o ON e.officeCode = o.officeCode
+    WHERE o.city = 'Boston'
+""", conn)
+
+
+# ### Step 2
+# 
+# Recent downsizing and employee attrition have caused some mixups in office tracking and the company is worried they are supporting a 'ghost' location. Are there any offices that have zero employees?
+
+# In[3]:
+
+
+# CodeGrade step2
+df_zero_emp = pd.read_sql("""
+    SELECT o.officeCode, o.city
+    FROM offices o
+    LEFT JOIN employees e ON o.officeCode = e.officeCode
+    WHERE e.employeeNumber IS NULL
+""", conn)
+
+
+# ## Part 2: Type of Join
+
+# ### Step 3
+# 
+# As a part of this larger analysis project the HR department is taking the time to audit employee records to make sure nothing is out of place and have asked you to produce a report of all employees. Return the employees first name and last name along with the city and state of the office that they work out of (if they have one). Include all employees and order them by their first name, then their last name.
+
+# In[4]:
+
+
+# CodeGrade step3
+df_employee = pd.read_sql("""
+    SELECT e.firstName, e.lastName, o.city, o.state
+    FROM employees e
+    LEFT JOIN offices o ON e.officeCode = o.officeCode
+    ORDER BY e.firstName, e.lastName
+""", conn)
+
+
+# ### Step 4
+# The customer management and sales rep team know that they have several 'customers' in the system that have not placed any orders. They want to reach out to these customers with updated product catalogs to try and get them to place initial orders. Return all of the customer's contact information (first name, last name, and phone number) as well as their sales rep's employee number for any customer that has not placed an order. Sort the results alphabetically based on the contact's last name
+# 
+# There are several approaches you could take here, including a left join and filtering on null values or using a subquery to filter out customers who do have orders. In total there are 24 customers who have not placed an order.
 
 # In[5]:
 
 
-# CodeGrade step1
-# Replace None with your code
-df_no_moons = pd.read_sql("""SELECT * FROM planets WHERE num_of_moons = 0""", conn1)
+# CodeGrade step4
+df_contacts = pd.read_sql("""
+    SELECT c.contactFirstName, c.contactLastName, c.phone, c.salesRepEmployeeNumber
+    FROM customers c
+    LEFT JOIN orders o ON c.customerNumber = o.customerNumber
+    WHERE o.orderNumber IS NULL
+    ORDER BY c.contactLastName
+""", conn)
 
 
-# ### Step 2
-# Return the name and mass of each planet that has a name with exactly 7 letters. Avoid hard coding this filter subset as much as possible.
+# ## Part 3: Built-in Function
+
+# ### Step 5
+# 
+# The accounting team is auditing their figures and wants to make sure all customer payments are in alignment, they have asked you to produce a report of all the customer contacts (first and last names) along with details for each of the customers' payment amounts and date of payment. They have asked that these results be sorted in descending order by the payment amount.
+# 
+# Hint: A member of their team mentioned that they are not sure the 'amount' column is being stored as the right datatype so keep this in mind when sorting.
 
 # In[6]:
 
 
-# CodeGrade step2
-# Replace None with your code
-df_name_seven = pd.read_sql("""SELECT name, mass FROM planets WHERE LENGTH(name) = 7""", conn1)
+# CodeGrade step5
+df_payment = pd.read_sql("""
+    SELECT c.contactFirstName, c.contactLastName, p.amount, p.paymentDate
+    FROM customers c
+    JOIN payments p ON c.customerNumber = p.customerNumber
+    ORDER BY CAST(p.amount AS REAL) DESC
+""", conn)
 
 
-# ## Part 2: Advanced Filtering
+# ## Part 4: Joining and Grouping
 
-# ### Step 3
+# ### Step 6
 # 
-# Return the name and mass for each planet that has a mass that is less than or equal to 1.00.
+# The sales rep team has noticed several key team members that stand out as having trustworthy business relations with their customers, reflected by high credit limits indicating more potential for orders. The team wants you to identify these 4 individuals. Return the employee number, first name, last name, and number of customers for employees whose customers have an average credit limit over 90k. Sort by number of customers from high to low.
 
 # In[7]:
 
 
-# CodeGrade step3
-# Replace None with your code
-df_mass = pd.read_sql("""SELECT name, mass FROM planets WHERE mass <= 1.00""", conn1)
+# CodeGrade step6
+df_credit = pd.read_sql("""
+    SELECT e.employeeNumber, e.firstName, e.lastName, COUNT(c.customerNumber) AS num_customers
+    FROM employees e
+    JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+    GROUP BY e.employeeNumber
+    HAVING AVG(c.creditLimit) > 90000
+    ORDER BY num_customers DESC
+    LIMIT 4
+""", conn)
 
 
-# ### Step 4
+# ### Step 7
 # 
-# Return all the columns for planets that have at least one moon and a mass less than 1.00.
+# The product team is looking to create new model kits and wants to know which current products are selling the most in order to get an idea of what is popular. Return the product name and count the number of orders for each product as a column named 'numorders'. Also return a new column, 'totalunits', that sums up the total quantity of product sold (use the quantityOrdered column). Sort the results by the totalunits column, highest to lowest, to showcase the top selling products.
 
 # In[8]:
 
 
-# CodeGrade step4
-# Replace None with your code
-df_mass_moon = pd.read_sql("""SELECT * FROM planets WHERE num_of_moons >= 1 AND mass < 1.00""", conn1)
+# CodeGrade step7
+df_product_sold = pd.read_sql("""
+    SELECT p.productName, COUNT(od.orderNumber) AS numorders, SUM(od.quantityOrdered) AS totalunits
+    FROM products p
+    JOIN orderdetails od ON p.productCode = od.productCode
+    GROUP BY p.productName
+    ORDER BY totalunits DESC
+""", conn)
 
 
-# ### Step 5
+# ## Part 5: Multiple Joins
+
+# ### Step 8
 # 
-# Return the name and color of planets that have a color containing the string "blue".
+# As a follow-up to the above question, the product team also wants to know how many different customers ordered each product to get an idea of market reach. Return the product name, code, and the total number of customers who have ordered each product, aliased as 'numpurchasers'. Sort the results by the highest  number of purchasers.
+# 
+# Hint: You might need to join more than 2 tables. Use DISTINCT to return unique/different values.
 
 # In[9]:
 
 
-# CodeGrade step5
-# Replace None with your code
-df_blue = pd.read_sql("""SELECT name, color FROM planets WHERE color LIKE '%blue%'""", conn1)
-
-
-# ## Part 3: Ordering and Limiting
-
-# This database has some fictional, yet generally famous, dogs.
-# 
-# Table Name: dogs
-
-# In[10]:
-
-
-# CodeGrade step0
-
-# Run this cell without changes
-
-# Create a connection
-# Note the connect is 'conn2' since they will be multiple .db used
-conn2 = sqlite3.connect('dogs.db')
-
-# Select all
-pd.read_sql("SELECT * FROM dogs;", conn2)
-
-
-# ### Step 6
-# Return the name, age, and breed of all dogs that are hungry (binary flag of 1) and sort them from youngest to oldest.
-
-# In[11]:
-
-
-# CodeGrade step6
-# Replace None with your code
-df_hungry = pd.read_sql("""SELECT name, age, breed FROM dogs WHERE hungry = 1 ORDER BY age ASC""", conn2)
-
-
-# ### Step 7
-# Return the name, age, and hungry columns for hungry dogs between the ages of two and seven. This query should also sort these dogs in alphabetical order.
-
-# In[12]:
-
-
-# CodeGrade step7
-# Replace None with your code
-df_hungry_ages = pd.read_sql("""SELECT name, age, hungry FROM dogs WHERE hungry = 1 AND age BETWEEN 2 AND 7 ORDER BY name ASC""", conn2)
-
-
-# ### Step 8
-# 
-# Return the name, age, and breed for the 4 oldest dogs. Sort the result alphabetically based on the breed.
-
-# In[13]:
-
-
 # CodeGrade step8
-# Replace None with your code
-df_4_oldest = pd.read_sql("""SELECT name, age, breed FROM dogs ORDER BY age DESC LIMIT 4""", conn2)
-
-
-# ## Part 4: Aggregation
-
-# In the next few parts, you'll query data from a table populated with Babe Ruth's career hitting statistics. You'll use aggregate functions to pull interesting information from the table that basic queries cannot track.
-# 
-# Table Name: babe_ruth_stats
-
-# In[16]:
-
-
-# CodeGrade step0
-
-# Run this cell without changes
-
-# Create a connection
-# Note the connect is 'conn3' since they will be multiple .db used
-conn3 = sqlite3.connect('babe_ruth.db')
-
-# Select all
-pd.read_sql("""
-SELECT * FROM babe_ruth_stats; """, conn3)
+df_total_customers = pd.read_sql("""
+    SELECT p.productName, p.productCode, COUNT(DISTINCT o.customerNumber) AS numpurchasers
+    FROM products p
+    JOIN orderdetails od ON p.productCode = od.productCode
+    JOIN orders o ON od.orderNumber = o.orderNumber
+    GROUP BY p.productCode
+    ORDER BY numpurchasers DESC
+""", conn)
 
 
 # ### Step 9
 # 
-# Return the total number of years that Babe Ruth played professional baseball
+# The custom relations team is worried they are not staffing locations properly to account for customer volume. They want to know how many customers there are per office. Return the count as a column named 'n_customers'. Also return the office code and city.
 
-# In[18]:
+# In[10]:
 
 
 # CodeGrade step9
-# Replace None with your code
-df_ruth_years = pd.read_sql("""SELECT COUNT(year) AS total_years FROM babe_ruth_stats""", conn3)
+df_customers = pd.read_sql("""
+    SELECT o.officeCode, o.city, COUNT(c.customerNumber) AS n_customers
+    FROM offices o
+    JOIN employees e ON o.officeCode = e.officeCode
+    JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+    GROUP BY o.officeCode
+""", conn)
 
+
+# ## Part 6: Subquery
 
 # ### Step 10
 # 
-# Return the total number of homeruns hit by Babe Ruth during his career.
+# Having looked at the results from above, the product team is curious to dig into the underperforming products. They want to ask members of the team who have sold these products about what kind of messaging was successful in getting a customer to buy these specific products. Using a subquery or common table expression (CTE), select the employee number, first name, last name, city of the office, and the office code for employees who sold products that have been ordered by fewer than 20 customers.
+# 
+# Hint: Start with the subquery, find all the products that have been ordered by 19 or less customers, consider adapting one of your previous queries.
 
-# In[19]:
+# In[11]:
 
 
 # CodeGrade step10
-# Replace None with your code
-df_hr_total = pd.read_sql("""SELECT SUM(hr) AS total_homeruns FROM babe_ruth_stats""", conn3)
+df_under_20 = pd.read_sql("""
+    SELECT DISTINCT e.employeeNumber, e.firstName, e.lastName, o.city, o.officeCode
+    FROM employees e
+    JOIN offices o ON e.officeCode = o.officeCode
+    JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+    JOIN orders ord ON c.customerNumber = ord.customerNumber
+    JOIN orderdetails od ON ord.orderNumber = od.orderNumber
+    WHERE od.productCode IN (
+        SELECT od2.productCode
+        FROM orderdetails od2
+        JOIN orders o2 ON od2.orderNumber = o2.orderNumber
+        GROUP BY od2.productCode
+        HAVING COUNT(DISTINCT o2.customerNumber) < 20
+    )
+""", conn)
 
 
-# ## Part 5: Grouping and Aggregation
+# ### Close the connection
 
-# ### Step 11
-# 
-# For each team that Babe Ruth has played on, return the team name and the number of years he played on that team, aliased as 'number_years'.
-
-# In[20]:
-
-
-# CodeGrade step11
-# Replace None with your code
-df_teams_years = pd.read_sql("""SELECT team, COUNT(year) AS number_years FROM babe_ruth_stats GROUP BY team""", conn3)
-
-
-# ### Step 12
-# 
-# For each team that Babe Ruth played on and averged over 200 at bats with, return the team name and average number of at bats, aliased as 'average_at_bats'.
-
-# In[21]:
-
-
-# CodeGrade step12
-# Replace None with your code
-df_at_bats = pd.read_sql("""SELECT team, AVG(at_bats) AS average_at_bats FROM babe_ruth_stats GROUP BY team HAVING AVG(at_bats) > 200""", conn3)
-
-
-# #### Close the connections
-
-# In[22]:
+# In[12]:
 
 
 # Run this cell without changes
 
-conn1.close()
-conn2.close()
-conn3.close()
-
-
-# In[ ]:
-
-
-
+conn.close()
 
